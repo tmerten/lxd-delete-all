5624e221f36f34d978a842624d5bc7e64df23dae
Thorsten Merten <thorsten.merten@canonical.com>
2022-11-15 15:54:16 +0100

    feat: supercharge with ability to delete more
    * add ability to cycle through all projects
      * add ability to leave alone/exclude projects
    * add ability to delete profiles
    * add ability to delete networks
    * --delete-all deletes containers, images, profiles, networks

--- README.md
+++ README.md
@@ -1,43 +1,40 @@
 # lxd-delete-containers
 
-The Python script in this repo can delete all [LXD](https://linuxcontainers.org/lxd/) containers and images on a machine. This script was inspired by the following [Docker](https://www.docker.com/) command that can delete all stopped containers: 
-```
-docker rm $(docker ps -f status=exited -q)
-```
-As you may be already aware, the above command finds stopped docker containers and deletes them. As of now, a similar command cannot be run in LXD since it cannot list containers in "quiet" mode (`-q` in the command above - where only numerical IDs of containers are listed, allowing them to be sent to the `rm` command). It is also not possible to delete all LXC containers using `lxc delete *`. This can be problematic if you have a large number of containers that are launched and not stopped on a machine, where you have to manually run the container delete command for each container (E.g.: `lxc delete awaited-mallard`).
+**Important: This script can mess up your system and screw up everytying you did with LXD. Use at your own risk!**
 
-The script `delete_all_lxd_containers.py` in this repo can delete **ALL** containers (running or stopped) and/or **ALL** images on a machine. Whether containers, images or both are deleted depends on what options are enabled. The script uses modules from the [Python Standard Library](https://docs.python.org/3/library/), mitigating the need to install additional modules for successful execution. 
+The Python script in this repo can delete all [LXD](https://linuxcontainers.org/lxd/) profiles, containers, networks, and images on a machine. It is basically an extension of [Ilhaan Rasheed's Script to delete containers and images](https://github.com/ilhaan/lxd-delete-containers).
 
-Enabling the container delete option is the equivalent to running the following command for each container: 
-```
-lxc delete --force <container_name> 
-```
-The `--force` option above forces the removal of running containers. 
+The main extension is, that everything can be deleted for all projects.
 
-Enabling the image delete option is the equivalent of running the following command for each image: 
+Resetting lxd entirely is the equivalent to running the following command for each container: 
 ```
+lxc stop <container_name>  # for all running containers
+lxc delete <container_name> 
 lxc image delete <image_fingerprint>
+lxc profile delete <profile_name>
+lxc network delete <network_name> # for all lxd managed networks
 ```
 
-This script has been tested on Ubuntu 16.04 & 18.04 with `snap` installed LXD versions 3.3 and above. 
+This script has been tested on Ubuntu 22.10 with a `snap` installed LXD version of 5.7. 
 
 ## Requirements 
+
 * Python3 
 * Ability to run `lxc` without `sudo`
 
 ## Instructions
+
 1. Clone this repo and `cd` into the cloned directory. 
-2. Make sure you have execute permissions on `delete_all_lxd_containers.py`. You can set this for the current user by running: `chmod u+x delete_all_lxd_containers.py`
-3. View available options: `./delete_all_lxd_containers.py --help`
-4. Delete containers: `./delete_all_lxd_containers.py -c`
-5. Delete images: `./delete_all_lxd_containers.py -i`
+2. Make sure you have execute permissions on `lxd_reset_all.py`. You can set this for the current user by running: `chmod u+x lxd_reset_all.py`
+3. View available options: `./lxd_reset_all.py --help`
+4. Reset everything: `./lxd_reset_all.py --reset-all`
+   - Exclude a project: `./lxd_reset_all.py --reset-all -e my_project`
+5. Delete containers: `./lxd_reset_all.py -c`
+6. Delete images: `./lxd_reset_all.py -i`
+7. Delete profiles: `./lxd_reset_all.py -p`
+8. Delete networks: `./lxd_reset_all.py -n`
 
 Alternatively, you can run this script without cloning the repo using [`curl`](https://curl.haxx.se/). For example, the following command runs the script and displays it's help message: 
 ```
-curl -s https://raw.githubusercontent.com/ilhaan/lxd-delete-containers/master/delete_all_lxd_containers.py | python3 - -h 
+curl -s https://raw.githubusercontent.com/tmerten/lxd-delete-all/master/lxd_delete_all.py | python3 - -h 
 ```
-
-## Alternatives
-The following commands will accomplish the same thing this script does, without needing Python or this script installed:
-* delete all containers:  `lxc delete $(lxc list -c n --format csv)`
-* delete all images:      `lxc image delete $(lxc image list -c f --format csv)`
---
+++ lxd_delete_all.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python3
+""" This script deletes all LXC containers, profiles, networks and images.
+
+This incldes running containers and OS images and
+excludes default profiles.
+
+The script assumes that the user running the script has non-sudo access
+to lxc
+
+No dependencies.
+
+Author: Thorsten Merten
+Author: Ilhaan Rasheed
+"""
+
+from typing import Any
+import json
+import subprocess
+import argparse
+import sys
+
+app_description = """
+This script can reset your entire LXD configuration to give you a fresh start.
+
+You can also use it to delete particular parts (e.g. containers, 
+images, networks, profiles) from LXD by using the corresponding switches.
+
+The script is very destructive if you use the --delete-all flag. Please
+consider excluding projects from resetting (using the -e switch).
+
+For all the other flags, the script tries to delete what is specified
+but does not force deletion.
+"""
+
+def get_args():
+    """Get args from command line"""
+
+    parser = argparse.ArgumentParser(
+            description=app_description,
+            formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--delete-all', dest='reset',
+                        default=False, action='store_true',
+                        help='Enable this to delete everything in LXD.\n' \
+                                'Exceptions are the default project and ' \
+                                'default profiles')
+    parser.add_argument('-c', '--containers', dest='container_delete',
+                        default=False, action='store_true',
+                        help='Enable this to delete all LXD containers')
+    parser.add_argument('-i', '--images', dest='image_delete',
+                        default=False,
+                        action='store_true',
+                        help='Enable this to delete all LXD images')
+    parser.add_argument('-p', '--profiles', dest='profile_delete',
+                        default=False,
+                        action='store_true',
+                        help='Enable this to delete all LXD profiles')
+    parser.add_argument('-n', '--newworks', dest='network_delete',
+                        default=False,
+                        action='store_true',
+                        help='Enable this to delete all LXD networks')
+    parser.add_argument('-e', '--exclude-projects',
+                        nargs="*", default=[], dest='exclude_projects',
+                        help='Do not touch the following projects\n'\
+                                f'E.g. {parser.prog} -e p1 proj2 --delete-all')
+    args = parser.parse_args()
+
+    if any(vars(args).values()):
+        return args
+    else:
+        print("No arguments given. Use --help for more information.")
+        sys.exit()
+
+class LxdCleaner:
+    """Encapsulates various LXD cleaning tasks and runs them for all
+    projects."""
+    def __init__(self, excluded_projects) -> None:
+        self.projects:list[str] = []
+        self._get_projects(excluded_projects)
+
+
+    @staticmethod
+    def _run_lxc_command(lxc_args:list[str], format_json:bool=False) -> Any:
+        """Run a lxc command"""
+        command = [ "lxc" ]
+        if format_json:
+            # unfortunately --format=json does not work for delete commands
+            command.append("--format=json")
+        command = command + lxc_args
+   
+        # run the lxc command and grab the output
+        lxc_output = subprocess.run(
+                command, stdout=subprocess.PIPE
+                ).stdout.decode('utf-8')
+
+        # Load json string output
+        if format_json:
+            return json.loads(lxc_output)
+        else:
+            return lxc_output
+
+
+    def _get_projects(self, excluded_projects):
+        data = self._run_lxc_command(["project", "list"], True)
+        for project in data:
+            if project not in excluded_projects:
+                self.projects.append(project['name'])
+
+
+    def reset(self):
+        """Function that resets/deletes everything"""
+        self.container_delete()
+        self.image_delete()
+        self.profile_delete()
+        self.network_delete()
+
+
+    def container_delete(self):
+        """Function that deletes all containers"""
+
+        for project in self.projects:
+            # Run lxc list command to get JSON output as a string
+            data = self._run_lxc_command([
+                "list", "--project", project,
+                 "--columns", "ns"
+                ], True)
+
+            # Create empty list to store LXC Container names
+            containers = []
+            for container in data:
+                containers.append((container["name"], container["state"]))
+
+            # Run LXD Delete command for each container
+            for container in containers:
+                if container[1] == 'running':
+                    # More gracefull, to stop first and delete afterwards
+                    subprocess.run([
+                        "/snap/bin/lxc",
+                        "--project", project,
+                        "stop", container[0]
+                        ])
+                subprocess.run([
+                    "/snap/bin/lxc", "--project", project,
+                     "delete", container[0]
+                    ])
+
+
+    def image_delete(self):
+        """Function that deletes all images"""
+
+        for project in self.projects:
+            # Run lxc image list command to get JSON output as a string
+            data = self._run_lxc_command([
+                "image", "list", "--project", project
+                ], True)
+
+            # Create empty list to store LXC Image fingerprints
+            image_fingerprints = []
+            for image in data:
+                image_fingerprints.append(image["fingerprint"])
+
+            # Run LXD Image Delete command for each container
+            for image in image_fingerprints:
+                self._run_lxc_command([
+                    "image", "--project", project, "delete", image
+                    ])
+
+
+    def profile_delete(self):
+        """Function that deletes all profiles"""
+
+        for project in self.projects:
+            # Run lxc image list command to get JSON output as a string
+            data = self._run_lxc_command([
+                "profile", "list", "--project", project
+                ], True)
+
+            # Run LXD profile Delete command for each non-default profile
+            for profile in data:
+                if profile['name'] != 'default':
+                    self._run_lxc_command([
+                        "profile", "--project", project,
+                        "delete", profile['name']
+                        ])
+
+    def network_delete(self):
+        """Function that deletes all lxd managed networks"""
+
+        for project in self.projects:
+            # Run lxc image list command to get JSON output as a string
+            data = self._run_lxc_command([
+                "network", "list", "--project", project
+                ], True)
+
+            for network in data:
+                if network['managed']:
+                    self._run_lxc_command([
+                        "network", "--project", project,
+                        "delete", network['name']
+                        ])
+
+if __name__ == '__main__':
+    args = get_args()
+    lc = LxdCleaner(args.exclude_projects)
+    
+    if args.reset:
+        lc.reset()
+    else:
+        if args.container_delete:
+            lc.container_delete()
+        if args.image_delete:
+            lc.image_delete()
+        if args.profile_delete:
+            lc.profile_delete()
+        if args.network_delete:
+            lc.network_delete()
